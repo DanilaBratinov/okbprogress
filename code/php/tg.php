d7000097a87ab3b32896435afdd08a08e9612a8b
DanilaBratinov <116026697+DanilaBratinov@users.noreply.github.com>
2024-08-09 13:46:10 +0300

    Test

--- code/php/tg.php
+++ code/php/tg.php
@@ -79,7 +79,7 @@ if (!$error) {
 
         $ch = curl_init($botUrl);
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "chat_id=-4258610788&text=" . urlencode($telegramMessage) . "&parse_mode=Markdown");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "chat_id=351661463&text=" . urlencode($telegramMessage) . "&parse_mode=Markdown");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $response = curl_exec($ch);
         curl_close($ch);
